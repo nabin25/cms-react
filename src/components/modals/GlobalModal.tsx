10c0439b8e4e0090a8262a1ead1b5dd053d9f2e4
Nabin Paudyal <paudyalnabin25@gmail.com>
2025-05-17 13:12:08 +0545

    Added close button on global modal

--- src/components/modals/GlobalModal.tsx
+++ src/components/modals/GlobalModal.tsx
@@ -1,6 +1,7 @@
 import { DialogTitle } from "@radix-ui/react-dialog";
-import { Dialog, DialogContent } from "../ui/dialog";
+import { Dialog, DialogContent, DialogFooter } from "../ui/dialog";
 import { useModalStore } from "../../stores/useModalStore";
+import { Button } from "../ui/button";
 
 export const GlobalModal = () => {
   const { view, title, isOpen, close } = useModalStore();
@@ -11,6 +12,11 @@ export const GlobalModal = () => {
       <DialogContent className="lg:max-w-screen-lg overflow-y-scroll max-h-screen">
         <h2 className="text-lg font-semibold">{title}</h2>
         {view}
+        <DialogFooter>
+          <div className="flex justify-end">
+            <Button onClick={() => close()}>Close</Button>
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
